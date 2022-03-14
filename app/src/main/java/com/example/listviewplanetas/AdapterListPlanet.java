de82e563644079b60cb1162b740deb75c70d7d33
alexhurahara <fernayxxx@gmail.com>
2022-03-14 10:08:52 -0600

    modificaion 1

--- app/src/main/java/com/example/listviewplanetas/AdapterListPlanet.java
+++ app/src/main/java/com/example/listviewplanetas/AdapterListPlanet.java
@@ -16,13 +16,15 @@ public class AdapterListPlanet extends ArrayAdapter<String> {
     private final Activity context;
     private final String[]  nombres;
     private final String[]  Descripcion;
+    private final String[]  detalles;
     private final Integer[]  id_imagenes;
 
-    public AdapterListPlanet(Activity context, String[]  nombres, String[] Descirpcion, Integer[] id_images) {
+    public AdapterListPlanet(Activity context, String[]  nombres, String[] Descirpcion,String[] detalles, Integer[] id_images) {
         super(context,R.layout.list_items,nombres);
         this.context = context;
         this.nombres = nombres;
         this.Descripcion = Descirpcion;
+        this.detalles = detalles;
         this.id_imagenes = id_images;
     }
 
@@ -32,10 +34,10 @@ public class AdapterListPlanet extends ArrayAdapter<String> {
         LayoutInflater inflater = context.getLayoutInflater();
         View itemView = inflater.inflate(R.layout.list_items,null);
         TextView _nombre = (TextView) itemView.findViewById(R.id.nombre);
-        TextView _precio = (TextView) itemView.findViewById(R.id.Descripcion);
+        TextView _descripcion = (TextView) itemView.findViewById(R.id.Descripcion);
         ImageView _imagen = (ImageView) itemView.findViewById(R.id.foto);
         _nombre.setText(nombres[position]);
-        _precio.setText(Descripcion[position]);
+        _descripcion.setText(Descripcion[position]);
         _imagen.setImageResource(id_imagenes[position]);
         return itemView;
     }
---
+++ app/src/main/java/com/example/listviewplanetas/Planetas.java
@@ -0,0 +1,30 @@
+package com.example.listviewplanetas;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Planetas extends AppCompatActivity {
+
+    private TextView NombrePlaneta;
+    private TextView DescripcionPlaneta;
+    private ImageView ImagenPlaneta;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.planetas);
+
+        NombrePlaneta = (TextView) findViewById(R.id.NombrePlaneta);
+        DescripcionPlaneta = (TextView) findViewById(R.id.DescripcionPlaneta);
+        ImagenPlaneta = (ImageView) findViewById(R.id.ImagenPlaneta);
+
+        String description = getIntent().getStringExtra("Descripcion");
+        String name = getIntent().getStringExtra("Nombre");
+
+        NombrePlaneta.setText(name);
+        DescripcionPlaneta.setText(description);
+    }
+}
